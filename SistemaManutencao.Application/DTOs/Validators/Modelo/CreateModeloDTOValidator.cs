d6750827117226f89699c710e97ff4b44c9aca22
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-06-20 09:00:54 -0300

    feat: sem rastreio de .vs

---
+++ SistemaManutencao.Application/DTOs/Validators/Modelo/CreateModeloDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SistemaManutencao.Application.DTOs.Entities.Modelo;
+
+namespace SistemaManutencao.Application.DTOs.Validators.Modelo
+{
+    public class CreateModeloDTOValidator : AbstractValidator<CreateModeloDTO>
+    {
+        public CreateModeloDTOValidator()
+        {
+            RuleFor(r => r.Nome)
+                .NotEmpty().WithMessage("Nome é obrigatório")
+                .MaximumLength(150).WithMessage("Nome deve ter no máximo 150 caracteres")
+                .MinimumLength(2).WithMessage("Nome deve ter no mínimo 2 caracteres");
+        }
+    }
+}
---
+++ SistemaManutencao.Application/DTOs/Validators/Modelo/UpdateCategoriaDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SistemaManutencao.Application.DTOs.Entities.Categoria;
+
+namespace SistemaManutencao.Application.DTOs.Validators.Modelo
+{
+    public class UpdateCategoriaDTOValidator : AbstractValidator<UpdateCategoriaDTO>    
+    {
+        public UpdateCategoriaDTOValidator()
+        {
+            RuleFor(r => r.Nome)
+                .NotEmpty().WithMessage("Nome é obrigatório")
+                .MaximumLength(150).WithMessage("Nome deve ter no máximo 150 caracteres")
+                .MinimumLength(2).WithMessage("Nome deve ter no mínimo 2 caracteres");
+
+            RuleFor(r => r.Descricao)
+                .NotEmpty().WithMessage("Descrição é obrigatória")
+                .MaximumLength(500).WithMessage("Descrição deve ter no máximo 500 caracteres")
+                .MinimumLength(2).WithMessage("Descrição deve ter no mínimo 2 caracteres");
+        }
+    }
+}
