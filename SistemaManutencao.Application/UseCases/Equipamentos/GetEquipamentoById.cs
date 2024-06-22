b571dc5fe51c664a3d4f88cda358d9494cf53c7e
BuildTools <tiago.sbittencourt@icloud.com>
2024-06-22 19:19:42 -0300

    fix

--- SistemaManutencao.Application/UseCases/Equipamentos/GetEquipamentoById.cs
+++ SistemaManutencao.Application/UseCases/Equipamentos/GetEquipamentoById.cs
@@ -1,6 +1,29 @@
-﻿namespace SistemaManutencao.Application.UseCases.Equipamentos
+﻿using AutoMapper;
+using SistemaManutencao.Application.DTOs.Entities.Equipamento;
+using SistemaManutencao.Domain.Exceptions;
+using SistemaManutencao.Domain.Interfaces.Repositories;
+
+namespace SistemaManutencao.Application.UseCases.Equipamentos
 {
     public class GetEquipamentoById
     {
+        private readonly IEquipamentoRepository _equipamentoRepository;
+        private readonly IMapper _mapper;
+
+        public GetEquipamentoById(IEquipamentoRepository equipamentoRepository, IMapper mapper)
+        {
+            _equipamentoRepository = equipamentoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<GetEquipamentoDTO> ExecuteAsync(Guid id)
+        {
+            var equipamento = await _equipamentoRepository.GetByIdAsync(id);
+
+            if (equipamento == null)
+                throw new EntidadeNaoEncontradaException("EX10006", "Equipamento não encontrado");
+
+            return _mapper.Map<GetEquipamentoDTO>(equipamento);
+        }
     }
 }
